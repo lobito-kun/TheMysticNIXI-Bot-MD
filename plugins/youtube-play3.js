2f411b2ea043050ed4b5f01f3834fb56652d7824
⛥TR0Y4NW0LF⛥ <63571538+lobito-kun@users.noreply.github.com>
2022-07-17 18:03:03 -0300

    Update youtube-play3.js

--- plugins/youtube-play3.js
+++ plugins/youtube-play3.js
@@ -1,4 +1,4 @@
-/*import { youtubeSearch } from '@bochilteam/scraper'
+import { youtubeSearch } from '@bochilteam/scraper'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
 if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴/𝚃𝙸𝚃𝚄𝙻𝙾 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Good Feeling - Flo Rida*`
 let vid = (await youtubeSearch(text)).video[0]
@@ -24,10 +24,9 @@ console.log(e)
 }}
 handler.command = /^play3|playdoc?$/i
 export default handler
-*/
 
 
-import { youtubeSearch } from '@bochilteam/scraper'
+/*import { youtubeSearch } from '@bochilteam/scraper'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
   if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
   let vid = (await youtubeSearch(text)).video[0]
@@ -51,3 +50,4 @@ handler.exp = 0
 
 
 export default handler
+*/
