e480c0f9140f209e53e33945606adef320a42975
⛥TR0Y4NW0LF⛥ <63571538+lobito-kun@users.noreply.github.com>
2022-07-17 01:12:21 -0300

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,4 +1,4 @@
-let media = './Menu2.jpg'
+/*let media = './Menu2.jpg'
 let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
 *Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The Shadow Borkers - Bot*
 
@@ -34,4 +34,4 @@ let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
 *15.-* https://chat.whatsapp.com/CMd00ZYfwXZHEoEIswHpv3
 `.trim(), wm, media, [['𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '.menu']], m)
 handler.command = /^linkgc|grupos$/i
-export default handler
+export default handler*/
