84b1e137f82a73e3668beaa36dd660679b0382f7
⛥TR0Y4NW0LF⛥ <63571538+lobito-kun@users.noreply.github.com>
2022-07-17 17:07:44 -0300

    Update buscador-lyrics.js

--- plugins/buscador-lyrics.js
+++ plugins/buscador-lyrics.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+/*import fetch from 'node-fetch'
 import { lyrics, lyricsv2 } from '@bochilteam/scraper'
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
@@ -34,4 +34,28 @@ console.log(e)
 handler.help = ['lirik','letra'].map(v => v + ' <Apa>')
 handler.tags = ['internet']
 handler.command = /^(lirik|lyrics|lyric|letra)$/i
+export default handler*/
+
+import { lyrics, lyricsv2 } from '@bochilteam/scraper'
+
+let handler = async (m, { conn, text, usedPrefix, command}) => {
+    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
+    if (!teks) throw `✳️ Ingrese el nombre de la canción`
+    const result = await lyricsv2(teks).catch(async _ => await lyrics(teks))
+    m.reply(`
+*${result.title}*
+${result.author}
+
+
+${result.lyrics}
+
+
+Link:  ${result.link}
+`.trim())
+}
+
+handler.help = ['Lyrics'].map(v => v + '')
+handler.tags = ['tools']
+handler.command = ['letra', 'lyrics', 'letras'] 
+
 export default handler
